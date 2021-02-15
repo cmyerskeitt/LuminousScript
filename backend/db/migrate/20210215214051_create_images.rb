3a56991566e383f38fc5194929bef44d530d4571
github username <github email address>
2021-02-15 17:31:17 -0500

    set up models, controllers, and routes with namespacing

---
+++ backend/db/migrate/20210215214051_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :images do |t|
+      t.string :image_url
+      t.string :caption
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
