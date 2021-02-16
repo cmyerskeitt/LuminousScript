3c58369e318941aa12fe335b513a462b42142dcf
github username <github email address>
2021-02-16 13:07:15 -0500

    create user route, create user create method, create user_params

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user, status: :accepted
+        else 
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
     
+    private 
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      # resources :users
+      resources :users, only: [:create, :show]
       resources :images, only: [:index, :create, :destroy]
     end
   end 
