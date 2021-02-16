178bf7f02dad63b6323d3c62e736569c2acc8d91
github username <github email address>
2021-02-16 09:32:51 -0500

    fixed syntax error in routes and models

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :users
-      # resources :images
+      resources :images
+    end
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Mom = User.create(name: 'Christine Myers Keitt')
-Dad= User.create(name: 'Micheal Myers Keitt')
-Toddler = User.create(name: 'Micheal Myers Keitt Jr.')
+mom = User.create(name: 'Christine Myers Keitt')
+dad= User.create(name: 'Micheal Myers Keitt')
+toddler = User.create(name: 'Micheal Myers Keitt Jr')
+
+Image.create(image_url: "https://images.unsplash.com/photo-1508205065970-cad01cebebb4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80", caption: "A mother's love", user_id: mom.id)
