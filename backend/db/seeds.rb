bf6d1fe98d3da79799863e5f9c0b737782024d3d
github username <github email address>
2021-02-16 10:56:50 -0500

    added dependent: :destroy, reset seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+User.destroy_all
+Image.destroy_all
 
 mom = User.create(name: 'Christine Myers Keitt')
 dad= User.create(name: 'Micheal Myers Keitt')
