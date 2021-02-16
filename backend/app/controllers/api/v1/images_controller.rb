beddf9f9815e35910a139b35ed58a7344b1e532b
github username <github email address>
2021-02-16 11:28:27 -0500

    created create method and image_params private method

--- backend/app/controllers/api/v1/images_controller.rb
+++ backend/app/controllers/api/v1/images_controller.rb
@@ -3,5 +3,15 @@ class Api::V1::ImagesController < ApplicationController
         images = Image.all
         render json: @images 
     end 
+
+    def create 
+        image = Image.new(image_params)
+    end 
+
+    private 
+
+    def image_params 
+        params.require(:image).permit(:image_url, :caption, :user_id)
+    end 
    
 end
