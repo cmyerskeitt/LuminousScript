ceaf92e20d998b15d75d43fdfac9cf7e97b896c7
github username <github email address>
2021-02-16 20:20:38 -0500

    add index routes to user

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :users, only: [:create, :show]
+      resources :users, only: [:index, :create, :show ]
       resources :images, only: [:index, :create, :show, :destroy]
     end
   end 
