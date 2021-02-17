4ae6e3b54a13d0c205924ca050275451cc095fb1
github username <github email address>
2021-02-16 20:19:20 -0500

    add user index, update uder create & user show

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,26 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def index
+        users = User.all
+        render json: ImageSerializer.new(users)
+    end
+    
+    
     def create
-        user = User.new(user_params)
-        if user.save
-            render json: user, status: :accepted
+        if user.find_by(:name => user_params[:name])
+            user = User.find_by(:name => user_params[:name])
+            redirect_to "/api/v1/users/#{user.id}"
         else 
-            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+           user = User.create(user_params)
+           render json: user
         end 
     end 
 
-    
+    def show 
+       user = User.find_by_id(:id => params[:id].to_i)
+       render json: user 
+    end 
+
     private 
 
     def user_params
