2763382a8ae28c2c1633cc30414ef59532e57925
github username <github email address>
2021-02-16 20:50:12 -0500

    debug user controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,23 +2,23 @@ class Api::V1::UsersController < ApplicationController
     
     def index
         users = User.all
-        render json: ImageSerializer.new(users)
+        render json: UserSerializer.new(users)
     end
     
     
     def create
-        if user.find_by(:name => user_params[:name])
+        if User.find_by(:name => user_params[:name])
             user = User.find_by(:name => user_params[:name])
             redirect_to "/api/v1/users/#{user.id}"
         else 
-           user = User.create(user_params)
-           render json: user
+            user = User.create(user_params)
+            render json: UserSerializer.new(user)
         end 
     end 
 
     def show 
-       user = User.find_by_id(:id => params[:id].to_i)
-       render json: user 
+       user = User.find_by(:id => params[:id].to_i)
+       render json: UserSerializer.new(user)
     end 
 
     private 
