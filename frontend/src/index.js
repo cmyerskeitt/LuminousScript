ce7997a5c250c35012090f0dff588d136bf561ec
github username <github email address>
2021-02-18 10:09:40 -0500

    create imageMarkup html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,15 @@ function getImages(){
     fetch(endPoint)
         .then(response => response.json())
         .then(images => {
-            console.log(images);
-    })
+            images.data.forEach(image => {
+                const imageMarkup = `
+                    <div data-id=${image.id}>
+                        <img src=${image.attributes.image_url} height="200" width="250">
+                        <h3>${image.attributes.caption}</h3>
+                        <p>${image.attributes.user.name}</p>
+                        <button data-id=${image.id}>delete</button>
+                    </div>`
+        
+            })
+        })
 }
